2ac3d7f27cca3107dbc7b38058658dec8b962974
bnl75 <diegoartu@hotmail.com>
2022-05-21 16:40:21 -0500

    Creo un endpoint con HTTP CODE 200 que regrese una lista de explorers

---
+++ express_api/app.js
@@ -0,0 +1,31 @@
+//Uso de objeto tipo Express:
+const express = require('express');
+
+//Creo app de Express:
+const app = express();
+
+//Defino el puerto en el que se va a montar la app: localhost:3000
+const port = 3000;
+
+//Indico que se van a usar JSON en las llamadas:
+app.use(express.json());
+
+//Creo un endpoint que regrese una lista de explorers (objetos) con HTTP METHODS:
+app.get('/v1/explorers', (req, res) => {
+    console.log(`API Explorers GET ALL requests ${new Date()}`);
+
+    const explorer1 = {id: 1, name: "Diego1"};
+    const explorer2 = {id: 2, name: "Diego2"};
+    const explorer3 = {id: 3, name: "Diego3"};
+    const explorer4 = {id: 4, name: "Diego4"};
+
+    const explorers = [explorer1, explorer2, explorer3, explorer4];
+
+    //Creo la respuesta a la petición con un HTTP CODE 200: 
+    res.status(200).json(explorers);
+});
+
+//Inicializo la app de Express:
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
